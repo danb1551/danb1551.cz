bf1f9f4ab1d2b139992c60abef1df260d2ac20e0
danb1551 <danielbalcar22@gmail.com>
2026-07-27 18:17:21 +0200

    my niga

---
+++ endpoints.md
@@ -0,0 +1,22 @@
+# all endpoint so that I don't need to remember or look it up
+###### also I know that this file is for AI for better navigation but I don't use AI for this so f*ck off (creating this file while listening anime opening songs)
+
+## / - [GET]
+
+ - index
+
+## /projects - [GET]
+
+ - projects
+
+## /cv - [GET]
+
+ - curricullum vitae
+
+## /health - [GET]
+
+ - redis healt check (created only for testing (yeah, that's my first project with redis))
+
+## /lookup - [GET]
+
+ - return {"lookup": Integer} where lookup is number of visitors of home page (I don't have mood to validate only unique visitors)
--- src/app.py
+++ src/app.py
@@ -1,26 +1,53 @@
-from flask import Flask, render_template, send_file, url_for
+from flask import Flask, render_template, send_file, url_for, request, jsonify, session
+import redis
+import os
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.secret_key = os.getenv("SECRET_KEY")
+
+    redis_cache = redis.Redis(
+        host="redis",
+        decode_responses=True
+    )
+    redis_cache.incr("visits")
 
     @app.errorhandler(404)
     def NotFound():
         return render_template("errors/404.html"), 404
 
-    @app.route('/', methods=["GET"])
+    @app.route("/", methods=["GET"])
     def home():
+        redis_cache.incr("visits")
         return render_template("index.html"), 200
 
-    @app.route('/projects', methods=["GET"])
+    @app.route("/projects", methods=["GET"])
     def projects():
         return render_template("projects.html"), 200
 
-    @app.route('/cv', methods=["GET"])
+    @app.route("/cv", methods=["GET"])
     def cv():
         return render_template("cv.html"), 200
-    
-    @app.route("/image/<nameOfTheImage>")
-    def imageDown(nameOfTheImage):
-        return send_file("templates/index.html", as_attachment=False, download_name="h.htm")
+
+    @app.route("/health")
+    def health_check():
+        """Verify Redis connection is working"""
+        if redis_cache.exists("gg") == 0:
+            return jsonify({'status': 'healthy', 'redis': 'connected'}), 200
+        else:
+            return jsonify({'status': 'unhealthy', 'redis': 'disconnected'}), 503
+
+    @app.route("/lookup")
+    def lookup():
+        return jsonify({'lookup': redis_cache.get("visits")}), 200
+
+    @app.route("/login")
+    def login():
+        if request.method == "GET":
+            return render_template("login.html")
+        # yeah, I know. Don't deserve heaven
+        elif request.method == "POST" and request.form.get("username") == os.getenv("ADMIN_USERNAME") and request.form.get("password") == os.getenv("ADMIN_PASSWORD"):
+            session["username"] = "ADMIN"
+            return "success - loged in", 200
 
     return app
